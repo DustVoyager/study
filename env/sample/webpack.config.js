7af14f627300f081943def47f6847101e6ac672f
DustVoyager <ohjamdol@gmail.com>
2025-02-08 23:22:17 +0900

    프론트엔드 개발환경의 이해와 실습 study - 3일차

---
+++ env/sample/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: { main: "./src/app.js" },
+  output: {
+    path: path.resolve("./dist"),
+    filename: "[name].js",
+  },
+};
