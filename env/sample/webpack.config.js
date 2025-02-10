9006c3d01b67187271c62f0cd0b1ec2e2cb64ed1
DustVoyager <ohjamdol@gmail.com>
2025-02-10 23:01:54 +0900

    css style loader

--- env/sample/webpack.config.js
+++ env/sample/webpack.config.js
@@ -10,8 +10,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        use: [path.resolve("./my-webpack-loader.js")],
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
