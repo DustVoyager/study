e91906a699ec0aa0f80199d926fe3161e3898016
DustVoyager <ohjamdol@gmail.com>
2025-02-11 20:50:58 +0900

    자주 사용하는 로더

--- env/sample/webpack.config.js
+++ env/sample/webpack.config.js
@@ -13,6 +13,15 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i, // 이미지 파일 처리
+        type: "asset", // 자동으로 `resource` 또는 `inline` 선택
+        parser: {
+          dataUrlCondition: {
+            maxSize: 20 * 1024, // 2KB 이하 이미지는 Base64 (2 * 1024 = 2048 바이트)
+          },
+        },
+      },
     ],
   },
 };
