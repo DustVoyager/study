44bedc51db5dd72d41570f03cb8a3560e7d474aa
DustVoyager <ohjamdol@gmail.com>
2025-02-12 23:11:18 +0900

    webpack loader 과제

--- env/practice/1-webpack-1-entry/webpack.config.js
+++ env/practice/1-webpack-1-entry/webpack.config.js
@@ -2,9 +2,11 @@ const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: { main: "./src/app.js" },
+  entry: {
+    main: "./src/app.js",
+  },
   output: {
-    path: path.resolve("./dist"),
     filename: "[name].js",
+    path: path.resolve("./dist"),
   },
 };
---
+++ env/practice/1-webpack-2-loader/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    main: "./src/app.js",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve("./dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 20 * 1024,
+          },
+        },
+      },
+    ],
+  },
+};
