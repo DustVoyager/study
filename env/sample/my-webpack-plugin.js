c5a9f82f2ebeb9fcc39bb8fce45325406ed4aa89
DustVoyager <ohjamdol@gmail.com>
2025-02-14 19:02:13 +0900

    plugin practice

--- env/sample/my-webpack-plugin.js
+++ env/sample/my-webpack-plugin.js
@@ -1,11 +1,34 @@
-const pluginName = "ConsoleLogOnBuildWebpackPlugin";
-
-class ConsoleLogOnBuildWebpackPlugin {
+class MyWebpackPlugin {
   apply(compiler) {
-    compiler.hooks.run.tap(pluginName, (compilation) => {
-      console.log("The webpack build process is starting!");
+    compiler.hooks.emit.tapAsync("MyWebpackPlugin", (compilation, callback) => {
+      const assetName = "main.js";
+      const asset = compilation.getAsset(assetName);
+
+      if (asset) {
+        const source = asset.source.source();
+
+        const banner = [
+          "/**",
+          " * 이것은 BannerPlugin이 처리한 결과입니다.",
+          " * Build Date: 2025-02-14",
+          " */",
+          "",
+        ].join("\n");
+
+        // Webpack 5에서는 updateAsset 사용
+        compilation.updateAsset(
+          assetName,
+          new compiler.webpack.sources.RawSource(banner + "\n" + source)
+        );
+
+        console.log(`✅ ${assetName}에 배너 추가 완료!`);
+      } else {
+        console.log(`❌ ${assetName} 파일을 찾을 수 없습니다.`);
+      }
+
+      callback();
     });
   }
 }
 
-module.exports = ConsoleLogOnBuildWebpackPlugin;
+module.exports = MyWebpackPlugin;
--- env/sample/webpack.config.js
+++ env/sample/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MyWebpackPlugin = require("./my-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -24,4 +25,5 @@ module.exports = {
       },
     ],
   },
+  plugins: [new MyWebpackPlugin()],
 };
