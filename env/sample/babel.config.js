5acfe30a7c90d085970dc715c8c7b06b9a000e21
DustVoyager <ohjamdol@gmail.com>
2025-02-18 22:31:18 +0900

    babel promis 해결

--- env/sample/babel.config.js
+++ env/sample/babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-  presets: ["@babel/preset-env"],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          // 여기다가 브라우저
+          chrome: "79",
+          ie: "11",
+        },
+        useBuiltIns: "usage",
+        corejs: {
+          version: 3,
+        },
+      },
+    ],
+  ],
 };
